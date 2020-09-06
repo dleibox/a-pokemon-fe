eb93912b929c982cc658d0b218069bba7488333d
dleibox <dleibox@gmail.com>
2020-09-05 23:43:35 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    pokeApi: 'http://pokeapi.dlei.ca/api/v1', // 'https://pokeapi.co/api/v2',
+    pokeApi: 'http://pokeapi2.dlei.ca/api/v2', // 'https://pokeapi.co/api/v2',
 };
